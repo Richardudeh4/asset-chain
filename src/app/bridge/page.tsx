f5430d222712d46fbb953a45e9f9850dcbd40c47
richardudeh4 <richardudeh03@gmail.com>
2025-04-05 11:00:57 +0100

    seveth commit

--- src/app/bridge/page.tsx
+++ src/app/bridge/page.tsx
@@ -11,7 +11,17 @@ import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { BuySelect } from './components/BuySelect';
 import { SelectAsset } from './components/SelectAsset';
+import { Separator } from '@/components/ui/separator';
+import polygon from "../../../public/assets/polygon.svg";
+import assetChain from "../../../public/assets/assetChain.svg";
+import greenCheck from "../../../public/assets/greenCheck.svg";
 
+
+const transactionCategory = [
+    {name:"All"},
+    {name:"Claimed"},
+    {name:"Unclaimed"},
+]
 
 const Bridge = () => {
   return (
@@ -35,7 +45,7 @@ const Bridge = () => {
                 </Button>
             </div>
         <div className='grid grid-cols-2 gap-2'>
-            <div className='py-4.5 px-5 col-span-[554.32px] border border-[#8298AF]  bg-[#1A2739] rounded-[10px] z-50 flex flex-col gap-3.5'>
+            <div className='py-4.5 px-5 col-span-[554.32px] border border-[#1A2739]  bg-[#070E17] rounded-[10px] z-50 flex flex-col gap-3.5'>
                 <div className='flex flex-row justify-between'>
                 <div className='flex flex-col gap-1 pl-2.5'>
                     <h1 className='text-[16px] leading-[145%] font-[450] font-circular'>From</h1>
@@ -114,9 +124,80 @@ const Bridge = () => {
                     <p className='text-center text-[#8298AF] font-[450] text-[12px]'>By confirming, you agree to Asset Chain’s <span className='text-[#2042B8]'>Terms of Use</span></p>
                 </div>
             </div>
-            <div className='py-3 px-5 border border-[#070E17] z-50 bg-[#1A2739] rounded-[10px]'>
-            <div className="flex flex-col gap-3">
-
+            <div className='py-3 px-5 border border-[#1A2739] z-30 bg-[#070E17] rounded-[10px]'>
+            <div className="flex flex-col gap-3 z-50">
+                <p className="text-[16px] font-[450] leading-[145%] font-circular text-white">Transaction History</p>
+                <p className="text-[#8298AF] text-[15px] ">View and claim bridged asset</p>
+                <Separator className="my-4 bg-[#1A2739]" />
+                <div className="px-1.5 py-2 flex flex-row justify-between items-center rounded-[54.11px] bg-[#030A13] overflow-scroll max-w-[313.1px]">
+                    {
+                        transactionCategory.map((item, index) => (
+                            <div key={index} className={`px-2 py-1 border border-transparent flex justify-center items-center ${index === 0 ? "bg-[#3CC9CD] text-[#000000] rounded-[66.18px] w-[84.64px]": ""}`}>
+                                <p className={`text-[#8298AF] ${index === 0 ? "text-black " : ""} text-[14px] font-[450] leading-[145%] font-circular`}>{item.name}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="px-3 bg-[#070E17] border border-[#1A2739] py-3 flex flex-col gap-5 rounded-[5px]">
+                    <div className="flex flex-row justify-between items-center">
+                        <div className="flex flex-row space-x-2.5">
+                            <div className="flex flex-row -space-x-5">
+                                <Image src={polygon} alt="polygon" width={42} height={42} className="z-20"/>
+                                <Image src={assetChain} alt="assetChain" width={42} height={42} className="z-40"/>
+                            </div>
+                            <div className="flex flex-col gap-0.5">
+                                <h1 className="text-[#8298AF] font-[450] text-[14px]">Bridged asset</h1>
+                                <h1 className="text-[#FFFFFF] text-[16px] font-[450]">340,345, USDC</h1>
+                            </div>
+                        </div>
+                        <h1 className="text-[#8298AF] text-[14px] font-[450]">17-05-2056 23:78</h1>
+                    </div>
+                    <div className="flex flex-row justify-between items-center">
+                        <div className="flex flex-col gap-1.5">
+                            <h1 className="text-[#8298AF] text-[14px] font-[450]">Block Verfication</h1>
+                            <div className="flex flex-row space-x-1 items-center">
+                                <h1 className="text-[16px] font-[450] font-circular">[ 100% ]</h1>
+                                <Image src={greenCheck} alt="greenCheck" width={16} height={16}/>
+                                <p className="text-[16px] pl-1 font-[450] text-[#00F482] font-circular">Verifed</p>
+                            </div>
+                        </div>
+                        <Button className="bg-[#2042B8] w-[139px] rounded-[25.26px]">
+                            Claim
+                        </Button>
+                    </div>
+                </div>
+                <div className="px-3 bg-[#070E17] border border-[#1A2739] py-3 flex flex-col gap-5 rounded-[5px]">
+                    <div className="flex flex-row justify-between items-center">
+                        <div className="flex flex-row space-x-2.5">
+                            <div className="flex flex-row -space-x-5">
+                                <Image src={polygon} alt="polygon" width={42} height={42} className="z-20"/>
+                                <Image src={assetChain} alt="assetChain" width={42} height={42} className="z-40"/>
+                            </div>
+                            <div className="flex flex-col gap-0.5">
+                                <h1 className="text-[#8298AF] font-[450] text-[14px]">Bridged asset</h1>
+                                <h1 className="text-[#FFFFFF] text-[16px] font-[450]">340,345, USDC</h1>
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                        <h1 className="text-[#8298AF] text-[14px] font-[450]">17-05-2056 23:78</h1>
+                        <Button className="font-italic text-[14px] font-[450] text-[#5CFFF3] bg-[#040A13] w-[86px] ml-8 rounded-[22px]">Trx Hash</Button>
+                        </div>
+                      
+                    </div>
+                    <div className="flex flex-row justify-between items-center">
+                        <div className="flex flex-col gap-1.5">
+                            <h1 className="text-[#8298AF] text-[14px] font-[450]">Block Verfication</h1>
+                            <div className="flex flex-row space-x-1 items-center">
+                                <h1 className="text-[16px] font-[450] font-circular">[ 100% ]</h1>
+                                <Image src={greenCheck} alt="greenCheck" width={16} height={16}/>
+                                <p className="text-[16px] pl-1 font-[450] text-[#00F482] font-circular">Verifed</p>
+                            </div>
+                        </div>
+                        <Button className="bg-[#070E17] w-[139px] rounded-[25.26px] border border-[#213040]">
+                            Claimed
+                        </Button>
+                    </div>
+                </div>
             </div>
             </div>
         </div>
